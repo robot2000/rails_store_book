8474dbe2e6a39d4aa5e737a02188321d5b616c1c
robot2000 <balickiy@yandex.ru>
2015-04-17 13:28:35 +0300

    fix1_12 seed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
 
   mount_uploader :image, ProductImageUploader
+
   has_many :line_items
 
   before_destroy :ensure_not_referenced
@@ -8,7 +9,7 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :image_url, allow_blank: true, format: {
+  validates :image, allow_blank: true, format: {
     with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create!(title: 'The Ruby language',
+  description:
+    %{<p>
+        The Ruby programming language D.Flanagan Y.Matsumoto 2008
+      </p>},
+  image:   'book1.jpg',
+  price: 90.00)
+# . . .
+Product.create!(title: 'Icebreaker',
+  description:
+    %{<p>
+        Ledokol Viktor Suvorov 1999
+      </p>},
+  image: 'book2.jpg',
+  price: 109.95)
+# . . .
+
+Product.create!(title: 'Airport',
+  description:
+    %{<p>
+        Airport Arttur Hailey 1995
+      </p>},
+  image: 'book3.jpg',
+  price: 80.00)
+
+Product.create!(title: 'Das Kapital',
+  description:
+    %{<p>
+        Das Kapital Karl Marks 2001
+      </p>},
+  image: 'book4.jpg',
+  price: 130.00)
+
+Product.create!(title: 'The Kobzar',
+  description:
+    %{<p>
+        The Kobzar Taras Shevchenko 2010
+      </p>},
+  image: 'book5.jpg',
+  price: 200.00)
+
+Product.create!(title: 'Stalker',
+  description:
+    %{<p>
+        Stalker Strugatskie brothers
+      </p>},
+  image: 'book6.jpg',
+  price: 85.00)
